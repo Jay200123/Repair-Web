600c160a2dd49521c13077287ca04ec3cef87e29
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-05 22:41:15 +0800

    Add  new route paths for repairs table & create table form

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { MainLayout } from "@layouts";
-import { RepairsTable } from "@pages";
+import { RepairsTable, CreateRepair, Dashboard } from "@pages";
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route element={<MainLayout />}>
-        <Route index element={<RepairsTable />} />
+        <Route index element={<Dashboard/>}/>
+        <Route path="/repairs" element={<RepairsTable />} />
+        <Route path="/create/repair" element={<CreateRepair />} />
       </Route>
     </Route>
   )
