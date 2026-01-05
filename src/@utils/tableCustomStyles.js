4f7f600202737f6d9e483397fba64401f740f88b
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-05 21:21:22 +0800

    Add tableCustomStyles for React-Datatable

---
+++ src/@utils/tableCustomStyles.js
@@ -0,0 +1,21 @@
+export const tableCustomStyles = {
+  headCells: {
+    style: {
+      backgroundColor: "#c9a128",
+      fontSize: "15px",
+      color: "#ffffff",
+    },
+  },
+  cells: {
+    style: {
+      backgroundColor: "#ffffff",
+      fontSize: "15px",
+      borderBottom: "1px solid #b3b3b3",
+    },
+    table: {
+      style: {
+        backgroundColor: "transparent",
+      },
+    },
+  },
+};
