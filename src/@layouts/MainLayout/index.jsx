fdfb45a074f4a590561b44a06e60886ac05a44ce
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-06 22:16:40 +0800

    Wrap layout in motion's AnimatePresence for page Transition

--- src/@layouts/MainLayout/index.jsx
+++ src/@layouts/MainLayout/index.jsx
@@ -1,16 +1,22 @@
 import { Navbar, Footer } from "@components";
 import { Outlet } from "react-router-dom";
+import { AnimatePresence } from "motion/react";
+import { useLocation } from "react-router-dom";
 
 export default function MainLayout() {
+  const location = useLocation();
+
   return (
     <>
-      <main className="min-h-screen flex flex-col">
-        <Navbar />
-        <div>
-          <Outlet />
-        </div>
-        <Footer />
-      </main>
+      <AnimatePresence mode="wait">
+        <main className="min-h-screen flex flex-col">
+          <Navbar />
+          <div key={location.pathname}>
+            <Outlet />
+          </div>
+          <Footer />
+        </main>
+      </AnimatePresence>
     </>
   );
 }
