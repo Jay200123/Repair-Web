c3a666190ee760d167ce6cbd288396f1fcb8cfe8
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-06 22:14:59 +0800

    Add MotionWrapper for each  react routes for page transition animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
+import { MotionWrapper } from "@components";
 import { MainLayout } from "@layouts";
 import { RepairsTable, CreateRepair, Dashboard } from "@pages";
 
@@ -11,9 +12,27 @@ const Router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route element={<MainLayout />}>
-        <Route index element={<Dashboard/>}/>
-        <Route path="/repairs" element={<RepairsTable />} />
-        <Route path="/create/repair" element={<CreateRepair />} />
+        <Route index element={<Dashboard />} />
+
+        {/* Repairs Table */}
+        <Route
+          path="/repairs"
+          element={
+            <MotionWrapper>
+              <RepairsTable />
+            </MotionWrapper>
+          }
+        />
+
+        {/* Repairs Forms  */}
+        <Route
+          path="/create/repair"
+          element={
+            <MotionWrapper>
+              <CreateRepair />
+            </MotionWrapper>
+          }
+        />
       </Route>
     </Route>
   )
