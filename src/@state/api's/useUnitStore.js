638ba2152fba910e97d11516779831819f3c0869
Renyel Jay Sioc <kenken.ayonon@gmail.com>
2026-01-07 22:46:38 +0800

    Add useUnitStore custom hook

---
+++ src/@state/api's/useUnitStore.js
@@ -0,0 +1,11 @@
+export const useUnitStore = (_set) => ({
+  getAllUnits: async () => {},
+
+  getUnitById: async () => {},
+
+  createUnit: async () => {},
+
+  updateUnit: async () => {},
+
+  deleteUnit: async () => {},
+});
